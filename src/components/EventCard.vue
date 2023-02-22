a2927570341a7323ad61b5de14ef874f6bae0761
Bak David <david@webgurus.biz>
2023-02-22 17:13:45 +0200

    Added axios API call to get data, created service for calls

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,32 +1,22 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
-    </div></router-link
-  >
+      <BaseIcon name="users">
+        <span>{{ event.attendees.length }} attending</span></BaseIcon
+      >
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 19, 2023',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'David Bak' },
-          { id: 'abc124', name: 'George Zak' },
-        ],
-      },
-    }
+  props: {
+    event: Object,
   },
 }
 </script>

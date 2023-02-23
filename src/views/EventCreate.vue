c330954da11aaa97401943fcafcd3c579e1db416
Bak David <david@webgurus.biz>
2023-02-23 12:42:34 +0200

    State & Getters with store

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <h1>Event Create, {{ user.name }}</h1>
+    <p>Event Create, {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>Event {{ getEventById(1) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    // getEvent() {
+    //   return this.$store.getters.getEventById
+    // },
+    // catLength() {
+    //   return this.$store.getters.catLength
+    // },
+    ...mapGetters(['getEventById', 'catLength']),
+    ...mapState(['user', 'categories']),
+  },
+}
 </script>
 
 <style></style>

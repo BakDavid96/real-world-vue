0d65f13e6afde6dd63d1ae4aefd38660ee063e60
Bak David <david@webgurus.biz>
2023-02-24 15:23:08 +0200

    Added pagination, used mapState and removed 1 extra API call

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,30 +3,42 @@
     <h1>Event Listing</h1>
 
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Previous Page</router-link
+      >
+    </template>
+    <template v-if="page * 3 < this.$store.state.eventsTotal">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        >Next Page</router-link
+      >
+    </template>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard,
   },
-  data() {
-    return {
-      events: [],
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log('There was an error: ', error.response)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page,
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events']),
   },
 }
 </script>
